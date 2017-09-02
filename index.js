38a03612b728d83201aeb6cae104f5b7b7bb0d56
ralusek <ralusek@gmail.com>
2017-09-01 17:07:39 -0700

    Got rid of modifyNodes and instead increased the flexibility of walkNodes to allow onNode to return values.

--- index.js
+++ index.js
@@ -1,53 +1,39 @@
 'use strict';
 
-/**
- *
- */
-function walkNodes(node, onNode, meta) {
-  onNode(node, meta);
-  
-  meta = Object.assign({}, meta || {});
-  delete meta.isArrayItem;
-  delete meta.name;
-  
-  if (node.type === 'array') {
-    meta.isArrayItem = true;
-    walkNodes(node.items, onNode, meta);
-  }
-  else if (node.type === 'object') {
-    for (let prop in node.properties) {
-      meta.name = prop;
-      walkNodes(node.properties[prop], onNode, meta);
-    }
-  }
-};
-
-module.exports.walkNodes = walkNodes;
 
 
 /**
  *
  */
-function modifyNodes(node, onNode, meta) {
-  node = Object.assign({}, onNode(node, meta));
+function walkNodes(node, onNode, meta = {}) {
+  const { lineage = [] } = meta;
 
-  meta = Object.assign({}, meta || {});
-  delete meta.isArrayItem;
-  delete meta.name;
-  
+  const childMeta = {
+    lineage: [...lineage, node]
+  };
+
   if (node.type === 'array') {
-    meta.isArrayItem = true;
-    node.items = modifyNodes(node.items, onNode, meta);
+    childMeta.isArrayItem = true;
+    meta.childArrayItem = walkNodes(node.items, onNode, childMeta);
   }
   else if (node.type === 'object') {
-    node.properties = Object.assign({}, node.properties);
+    const required = new Set(node.required || []);
+
+    const childProps = meta.childObjectProperties = {};
     for (let prop in node.properties) {
-      meta.name = prop;
-      node.properties[prop] = modifyNodes(node.properties[prop], onNode, meta);
+      childMeta.name = prop;
+      childMeta.isRequired = required.has(prop);
+      childProps[prop] = walkNodes(node.properties[prop], onNode, childMeta);
     }
   }
 
-  return node;
+  return onNode(node, meta);
 }
 
-module.exports.modifyNodes = modifyNodes;
+
+/**
+ *
+ */
+module.exports = (node, onNode) => {
+  return walkNodes(node, onNode);
+};
